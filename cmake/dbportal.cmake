c5f70465b117a2d7cde1eb5d582b1f25d81eea16
VincentRisi <vincent.risi@gmail.com>
2015-03-30 14:26:24 +0200

    pushing up extras

--- cmake/dbportal.cmake
+++ cmake/dbportal.cmake
@@ -17,9 +17,8 @@ endfunction ()
 #  1: switch                     -  "OneSQLScript=1"
 #  2: target|dir|ext             -  "SO|${soDir}|.so"
 #  3: target|dir|ext|group|mask  -  "C|${shDir}|.sh|SH Files|.*[.]sh$" 
-#    target C      is changed to TargetC=1
-#    dir ${shDir}  is changed to CDir=${shDir}
-#    ext .sh       is changed to CExt=.sh 
+#     C|${shDir}|.sh      
+#     is changed to TargetC=1 CDir=${shDir} CExt=.sh 
 function (dbportal_split target_args)
   list (LENGTH target_args count)
   if (4 LESS count)
@@ -138,121 +137,129 @@ function (dbportal projectName binFilename siFiles)
   )
 endfunction()
 
-# function (jportal projectName siFiles)
-#   source_group ("SI Files"  REGULAR_EXPRESSION ".*[.]si$")
-#   set (switches)
-#   foreach (arg ${ARGN})
-#     list(APPEND switches "${arg}")
-#   endforeach ()
-#   set (sqlFiles)
-#   set (jportalJar ${TOOLS_DIR}/anydb/jportal.jar)
-#   foreach (siFile ${siFiles})
-#     get_filename_component (temp ${siFile} NAME)
-#     string (TOLOWER ${temp} temp1)
-#     get_filename_component (filename ${temp1} NAME_WE)
-#     set (sqlFile ${sqlDir}/${filename}.sql)
-#     list (APPEND sqlFiles ${sqlFile})
-#     add_custom_command (
-#       OUTPUT  ${sqlFile}
-#       COMMAND java -jar ${jportalJar} ${siFile} ${switches}
-#       DEPENDS ${siFile}
-#       VERBATIM
-#     )
-#   endforeach ()
-#   set (sqlFiles ${sqlFiles} PARENT_SCOPE)
-#   add_custom_target (${projectName} ALL
-#     DEPENDS ${sqlFiles} 
-#     SOURCES ${siFiles}
-#   )
-# endfunction()
-# 
-# function (idl2 projectName imFile iiDir ibDir)
-#   source_group ("IDL Source"  REGULAR_EXPRESSION ".*[.](im|ib)$")
-#   source_group ("IDL Built"   REGULAR_EXPRESSION ".*[.](ii|idl|idl2)$")
-#   set (switches)
-#   foreach (arg ${ARGN})
-#     list(APPEND switches "${arg}")
-#   endforeach ()
-#   get_filename_component(temp ${imFile} NAME)
-#   get_filename_component(filename ${temp} NAME_WE)
-#   set (idlFile ${CMAKE_CURRENT_BINARY_DIR}/idl/${filename}.idl2)
-#   set (crackleJar ${TOOLS_DIR}/idl2/crackle.jar)
-#   file (GLOB ibFiles ${ibDir}/*.ib)
-#   file (GLOB iiFiles ${iiDir}/*.ii)
-#   add_custom_command(
-#     OUTPUT ${idlFile}
-#     COMMAND java -jar ${crackleJar} -i ${iiDir} -b ${ibDir} -f ${idlFile} ${imFile} ${switches}
-#     DEPENDS ${imFile} ${ibFiles} ${iiFiles}
-#     VERBATIM
-#   )
-#   add_custom_target (${projectName} ALL
-#     DEPENDS ${idlFile} 
-#     SOURCES ${imFile} ${ibFiles} ${iiFiles} ${idlFile}
-#   )
-# endfunction()
-# 
-# function (idl2_simple projectName idlFile)
-#   source_group ("IDL Source"  REGULAR_EXPRESSION ".*[.](idl|idl2)$")
-#   set (crackleJar ${TOOLS_DIR}/idl2/crackle.jar)
-#   set (switches)
-#   foreach (arg ${ARGN})
-#     list(APPEND switches "${arg}")
-#   endforeach ()
-#   set (simpleTarget ${CMAKE_CURRENT_BINARY_DIR}/${projectName})
-#   if (SHOW_COMMAND)
-#     message (STATUS "COMMAND java -jar ${crackleJar} ${idlFile} ${switches}")
-#     message (STATUS "COMMAND echo \"built\" > ${simpleTarget}")
-#   endif ()
-#   add_custom_command(
-#     OUTPUT ${simpleTarget}
-#     COMMAND java -jar ${crackleJar} ${idlFile} ${switches}
-#     COMMAND echo "built" > ${simpleTarget}
-#     DEPENDS ${idlFile}
-#     VERBATIM
-#   )
-#   add_custom_target (${projectName} ALL
-#     DEPENDS ${simpleTarget}
-#     SOURCES ${idlFile}
-#   )
-# endfunction ()
-# 
-# function (pickle projectName pmFile piDir prDir)
-#   source_group ("PICKLE Source"  REGULAR_EXPRESSION ".*[.](pm|pr)$")
-#   source_group ("PICKLE Built"   REGULAR_EXPRESSION ".*[.](pi|pickle)$")
-#   set (switches)
-#   foreach (arg ${ARGN})
-#     list(APPEND switches "${arg}")
-#   endforeach ()
-#   get_filename_component(temp ${pmFile} NAME)
-#   get_filename_component(filename ${temp} NAME_WE)
-#   set (pickleFile ${CMAKE_CURRENT_BINARY_DIR}/pickle/${filename}.pickle)
-#   set (pickleJar ${TOOLS_DIR}/pickle/pickle.jar)
-#   file (GLOB prFiles ${prDir}/*.pr)
-#   file (GLOB piFiles ${piDir}/*.pi)
-#   add_custom_command(
-#     OUTPUT ${pickleFile}
-#     COMMAND java -jar ${pickleJar} -p ${piDir} -r ${prDir} -f ${pickleFile} ${pmFile} ${switches}
-#     DEPENDS ${pmFile} ${piFiles} ${prFiles}
-#     VERBATIM
-#   )
-#   add_custom_target (${projectName} ALL
-#     DEPENDS ${pickleFile}
-#     SOURCES ${pmFile} ${piFiles} ${prFiles} ${pickleFile}
-#   )
-# endfunction()
-# 
-# function (lite3sql dbFile sqlFiles)
-#   source_group ("SQL Files"  REGULAR_EXPRESSION ".*[.](sql)$")
-#   get_filename_component(dbDir  ${dbFile} PATH)
-#   get_filename_component(dbName ${dbFile} NAME)
-#   set (batchFile ${dbDir}/build.bat)
-#   file (WRITE ${batchFile} "rem - build of lite3\n")
-#   set (lite3SQL ${TOOLS_DIR}/sqllite3/lite3SQL.py)
-#   foreach (sqlFile ${sqlFiles})
-#     get_filename_component(temp ${sqlFile} NAME)
-#     get_filename_component(tableName ${temp} NAME_WE)
-#     set (logName ${dbDir}/${tableName}.log)
-#     set (cmd "${PYTHON_EXECUTABLE} ${lite3SQL} -d ${dbName} ${sqlFile}")
-#     file (APPEND ${batchFile} "${cmd}\n")
-#   endforeach()
-# endfunction ()
+function (jportal projectName siFiles)
+  source_group ("SI Files"  REGULAR_EXPRESSION ".*[.]si$")
+  set (switches)
+  foreach (arg ${ARGN})
+    list(APPEND switches "${arg}")
+  endforeach ()
+  set (sqlFiles)
+  set (jportalJar ${TOOLS_DIR}/anydb/jportal.jar)
+  foreach (siFile ${siFiles})
+    get_filename_component (temp ${siFile} NAME)
+    string (TOLOWER ${temp} temp1)
+    get_filename_component (filename ${temp1} NAME_WE)
+    set (sqlFile ${sqlDir}/${filename}.sql)
+    list (APPEND sqlFiles ${sqlFile})
+    add_custom_command (
+      OUTPUT  ${sqlFile}
+      COMMAND java -jar ${jportalJar} ${siFile} ${switches}
+      DEPENDS ${siFile}
+      VERBATIM
+    )
+  endforeach ()
+  set (sqlFiles ${sqlFiles} PARENT_SCOPE)
+  add_custom_target (${projectName} ALL
+    DEPENDS ${sqlFiles} 
+    SOURCES ${siFiles}
+  )
+endfunction()
+
+function (idl2 projectName imFile iiDir ibDir)
+  source_group ("IDL Source"  REGULAR_EXPRESSION ".*[.](im|ib)$")
+  source_group ("IDL Built"   REGULAR_EXPRESSION ".*[.](ii|idl|idl2)$")
+  set (switches)
+  foreach (arg ${ARGN})
+    list(APPEND switches "${arg}")
+  endforeach ()
+  get_filename_component(temp ${imFile} NAME)
+  get_filename_component(filename ${temp} NAME_WE)
+  set (idlFile ${CMAKE_CURRENT_BINARY_DIR}/idl/${filename}.idl2)
+  set (crackleJar ${TOOLS_DIR}/idl2/crackle.jar)
+  file (GLOB ibFiles ${ibDir}/*.ib)
+  file (GLOB iiFiles ${iiDir}/*.ii)
+  add_custom_command(
+    OUTPUT ${idlFile}
+    COMMAND java -jar ${crackleJar} -i ${iiDir} -b ${ibDir} -f ${idlFile} ${imFile} ${switches}
+    DEPENDS ${imFile} ${ibFiles} ${iiFiles}
+    VERBATIM
+  )
+  add_custom_target (${projectName} ALL
+    DEPENDS ${idlFile} 
+    SOURCES ${imFile} ${ibFiles} ${iiFiles} ${idlFile}
+  )
+endfunction()
+
+function (idl2_simple projectName idlFile)
+  source_group ("IDL Source"  REGULAR_EXPRESSION ".*[.](idl|idl2)$")
+  set (crackleJar ${TOOLS_DIR}/idl2/crackle.jar)
+  set (switches)
+  foreach (arg ${ARGN})
+    list(APPEND switches "${arg}")
+  endforeach ()
+  set (simpleTarget ${CMAKE_CURRENT_BINARY_DIR}/${projectName})
+  if (SHOW_COMMAND)
+    message (STATUS "COMMAND java -jar ${crackleJar} ${idlFile} ${switches}")
+    message (STATUS "COMMAND echo \"built\" > ${simpleTarget}")
+  endif ()
+  add_custom_command(
+    OUTPUT ${simpleTarget}
+    COMMAND java -jar ${crackleJar} ${idlFile} ${switches}
+    COMMAND echo "built" > ${simpleTarget}
+    DEPENDS ${idlFile}
+    VERBATIM
+  )
+  add_custom_target (${projectName} ALL
+    DEPENDS ${simpleTarget}
+    SOURCES ${idlFile}
+  )
+endfunction ()
+
+function (pickle projectName pmFile piDir prDir)
+  source_group ("PICKLE Source"  REGULAR_EXPRESSION ".*[.](pm|pr)$")
+  source_group ("PICKLE Built"   REGULAR_EXPRESSION ".*[.](pi|pickle)$")
+  set (switches)
+  foreach (arg ${ARGN})
+    list(APPEND switches "${arg}")
+  endforeach ()
+  get_filename_component(temp ${pmFile} NAME)
+  get_filename_component(filename ${temp} NAME_WE)
+  set (pickleFile ${CMAKE_CURRENT_BINARY_DIR}/pickle/${filename}.pickle)
+  set (pickleJar ${TOOLS_DIR}/pickle/pickle.jar)
+  file (GLOB prFiles ${prDir}/*.pr)
+  file (GLOB piFiles ${piDir}/*.pi)
+  add_custom_command(
+    OUTPUT ${pickleFile}
+    COMMAND java -jar ${pickleJar} -p ${piDir} -r ${prDir} -f ${pickleFile} ${pmFile} ${switches}
+    DEPENDS ${pmFile} ${piFiles} ${prFiles}
+    VERBATIM
+  )
+  add_custom_target (${projectName} ALL
+    DEPENDS ${pickleFile}
+    SOURCES ${pmFile} ${piFiles} ${prFiles} ${pickleFile}
+  )
+endfunction()
+
+function (lite3sql dbFile sqlFiles)
+  source_group ("SQL Files"  REGULAR_EXPRESSION ".*[.](sql)$")
+  get_filename_component(dbDir  ${dbFile} PATH)
+  get_filename_component(dbName ${dbFile} NAME)
+  set (batchFile ${dbDir}/build.bat)
+  file (WRITE ${batchFile} "rem - build of lite3\n")
+  set (lite3SQL ${TOOLS_DIR}/sqllite3/lite3SQL.py)
+  foreach (sqlFile ${sqlFiles})
+    get_filename_component(temp ${sqlFile} NAME)
+    get_filename_component(tableName ${temp} NAME_WE)
+    set (logName ${dbDir}/${tableName}.log)
+    set (cmd "${PYTHON_EXECUTABLE} ${lite3SQL} -d ${dbName} ${sqlFile}")
+    file (APPEND ${batchFile} "${cmd}\n")
+  endforeach()
+endfunction ()
+
+function (install_file files destdir)
+  file (INSTALL ${files} DESTINATION ${destdir} FILE_PERMISSIONS OWNER_WRITE OWNER_READ GROUP_READ GROUP_WRITE WORLD_READ)
+endfunction ()  
+
+function (install_exec files destdir)
+  file (INSTALL ${files} DESTINATION ${destdir} FILE_PERMISSIONS OWNER_WRITE OWNER_READ OWNER_EXECUTE GROUP_READ GROUP_WRITE GROUP_EXECUTE WORLD_READ)
+endfunction ()  
